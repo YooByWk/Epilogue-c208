8c6ab328833451f1e2c833efe6dbf4c8f0f3fe18
kanagyi95 <kanagyi95@gmail.com>
2024-03-15 17:42:22 +0900

    Git: push
    Former-commit-id: a3b8d3c6f068e22ebb5d7909b7a5515ecf44331d

--- frontend/lib/routes/main_route.dart
+++ frontend/lib/routes/main_route.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 import 'package:frontend/screens/login/login_screen.dart';
 import 'package:frontend/screens/memorial/memorial_main/memorial_screen.dart';
-// import 'package:frontend/screens/main/main_screen.dart';
+import 'package:frontend/screens/main/main_screen.dart';
 import 'package:frontend/screens/memorial/memorial_detail/memorial_detail_screen.dart';
 
 
 final Map<String, WidgetBuilder> mainRoutes = {
   '/login': (context) => LoginScreen(),
   '/memorial': (context) => MemorialScreen(),
-  // '/main' : (context) => MainScreen(),
+  '/main' : (context) => MainScreen(),
 
 };
 
@@ -25,8 +25,9 @@ Route<dynamic> generateMainRoute(RouteSettings settings) {
     case '/memorial':
       return MaterialPageRoute(builder: (context) => MemorialScreen());
 
-    // case '/main':
-    //   return MaterialPageRoute(builder: (context) => MainScreen());
+    case '/main':
+      return MaterialPageRoute(builder: (context) => MainScreen());
+
     case '/memorialDetail':
     
       return MaterialPageRoute(builder: (context) => MemorialDetailScreen(),
---
+++ frontend/lib/widgets/main_banner.dart
@@ -0,0 +1,33 @@
+// import 'package:carousel_slider/carousel_options.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:flutter/material.dart';
+//
+// class MainBanner extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) =>
+//       Column(
+//         children: <Widget>[
+//         CarouselSlider(
+//         items: child,
+//         carouselController: buttonCarouselController,
+//         options: CarouselOptions(
+//           autoPlay: false,
+//           enlargeCenterPage: true,
+//           viewportFraction: 0.9,
+//           initialPage: 1,
+//         ),
+//       )
+//
+//   ,
+//
+//   RaisedButton
+//
+//   (
+//
+//   onPressed: () => buttonCarouselController.nextPage(
+//   duration: DDuration(milliseconds: 300), curve: Curves.linear),
+//   child: Text('>'),
+//   )
+//   ]
+//   );
+// }
