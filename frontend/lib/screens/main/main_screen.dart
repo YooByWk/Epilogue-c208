2dfa413794230ea16d3d1f27d26a8be4ab5a0055
kanagyi95 <kanagyi95@gmail.com>
2024-03-15 14:33:09 +0900

    Design: 메인 설명 아이콘
    Former-commit-id: ed47d3a3b79e295c1401a3d8a8a7ba37693002e0

--- frontend/lib/screens/main/main_screen.dart
+++ frontend/lib/screens/main/main_screen.dart
@@ -1,28 +1,53 @@
-// import 'package:flutter/material.dart';
-// import 'package:frontend/widgets/main_willicon_widget.dart';
-//
-// class MainScreen extends StatelessWidget {
-//   const MainScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               MainWilliconWidget(
-//                 text: '자필증서',
-//                 imagePath: 'assets/images/letter.png',
-//             ),
-//           ),
-//         ],
-//       ),
-//       SizedBox(height: 10),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:frontend/main.dart';
+import 'package:frontend/widgets/main_willicon_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MainWilliconWidget(
+                  text: '자필증서',
+                  imagePath: 'assets/images/letter.png',
+                ),
+                MainWilliconWidget(
+                  text: '녹음',
+                  textColor: themeColour5,
+                  borderColor: themeColour5,
+                  imagePath: 'assets/images/voice.png',
+                ),
+                MainWilliconWidget(
+                  text: '공정증서',
+                  imagePath: 'assets/images/jury.png',
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MainWilliconWidget(
+                  text: '비밀증서',
+                  imagePath: 'assets/images/secret.png',
+                ),
+                MainWilliconWidget(
+                  text: '구수증서',
+                  imagePath: 'assets/images/emergency.png',
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
