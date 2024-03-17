2bffdec8d7cb445a3ee2fdab0c1abd8885217839
kanagyi95 <kanagyi95@gmail.com>
2024-03-17 16:40:20 +0900

    Design: banner button, text
    Former-commit-id: 4a6d05af99a9d4661391197109da23166c169dfa

--- frontend/lib/routes/main_route.dart
+++ frontend/lib/routes/main_route.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:frontend/screens/login/login_screen.dart';
 import 'package:frontend/screens/memorial/memorial_main/memorial_screen.dart';
 import 'package:frontend/screens/main/main_screen.dart';
 import 'package:frontend/screens/memorial/memorial_detail/memorial_detail_screen.dart';
-
+import 'package:frontend/screens/letter/letter_screen.dart';
+import 'package:frontend/screens/will/will_screen.dart';
 
 final Map<String, WidgetBuilder> mainRoutes = {
   '/login': (context) => LoginScreen(),
   '/memorial': (context) => MemorialScreen(),
-  '/main' : (context) => MainScreen(),
-
+  '/main': (context) => MainScreen(),
+  '/letter': (context) => LetterScreen(),
 };
-
-
-
 
 Route<dynamic> generateMainRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -28,11 +25,13 @@ Route<dynamic> generateMainRoute(RouteSettings settings) {
     case '/main':
       return MaterialPageRoute(builder: (context) => MainScreen());
 
+    case '/letter':
+      return MaterialPageRoute(builder: (context) => LetterScreen());
+
     case '/memorialDetail':
-    
-      return MaterialPageRoute(builder: (context) => MemorialDetailScreen(),
-      settings : RouteSettings(arguments: settings.arguments)
-      );
+      return MaterialPageRoute(
+          builder: (context) => MemorialDetailScreen(),
+          settings: RouteSettings(arguments: settings.arguments));
 
     default:
       debugPrint('Route Error');
--- frontend/lib/widgets/main_banner.dart
+++ frontend/lib/widgets/main_banner.dart
@@ -1,5 +1,21 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/common_button.dart';
+import 'package:frontend/main.dart';
+
+class BannerItem {
+  final String imagePath;
+  final String description;
+  final String routeName;
+  final String buttonText;
+
+  BannerItem({
+    required this.imagePath,
+    required this.description,
+    required this.routeName,
+    required this.buttonText,
+  });
+}
 
 class MainBannerWidget extends StatefulWidget {
   MainBannerWidget({super.key});
@@ -11,11 +27,31 @@ class MainBannerWidget extends StatefulWidget {
 class _MainBannerWidgetState extends State<MainBannerWidget> {
   int _current = 0;
   final CarouselController _controller = CarouselController();
-  final List<String> imageList = [
-    'assets/images/banner1.jpg',
-    'assets/images/banner2.jpg',
-    'assets/images/banner3.jpg',
-    'assets/images/banner4.jpg',
+  final List<BannerItem> banners = [
+    BannerItem(
+        imagePath: 'assets/images/banner1.jpg',
+        description: '블록체인을 활용한 디지털 유언장 서비스\n\n당신의 유언장을 남겨보세요.',
+        routeName: '/will',
+        buttonText: '유언장 기록하기',
+    ),
+    BannerItem(
+      imagePath: 'assets/images/banner2.jpg',
+      description: '사랑하는 가족, 친구들에게 마음을 전하세요.',
+      routeName: '/letter',
+      buttonText: '편지 남기기',
+    ),
+    BannerItem(
+      imagePath: 'assets/images/banner3.jpg',
+      description: '문자, 메일로 전달 받은 코드를 통해\n유언장을 열람할 수 있습니다.',
+      routeName: '',
+      buttonText: '유언 열람하기',
+    ),
+    BannerItem(
+      imagePath: 'assets/images/banner4.jpg',
+      description: '사랑하는 사람들과의 아름다운 추억을 간직하세요.',
+      routeName: '/memorial',
+      buttonText: '디지털 추모관',
+    ),
   ];
 
   @override
@@ -25,15 +61,51 @@ class _MainBannerWidgetState extends State<MainBannerWidget> {
       children: <Widget>[
         CarouselSlider(
           carouselController: _controller,
-          items: imageList.map((imagePath) {
+          items: banners.map((banner) {
             return Builder(
               builder: (context) {
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset(
-                    fit: BoxFit.fill,
-                    imagePath,
-                  ),
+                return Stack(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Opacity(
+                        opacity: 0.5,
+                        child: Image.asset(
+                          banner.imagePath,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 50,
+                      right: 0,
+                      left: 0,
+                      child: Text(
+                        banner.description,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 50,
+                        right: 0,
+                        left: 0,
+                        child: Center(
+                            child: SizedBox(
+                          width: 150,
+                          child: CommonButtonWidget(
+                            text: banner.buttonText,
+                            backgroundColor: themeColour5,
+                            onPressed: () {
+                              Navigator.pushNamed(context, banner.routeName);
+                            },
+                          ),
+                        ))),
+                  ],
                 );
               },
             );
@@ -54,10 +126,7 @@ class _MainBannerWidgetState extends State<MainBannerWidget> {
           bottom: 8.0,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: imageList
-                .asMap()
-                .entries
-                .map((entry) {
+            children: banners.asMap().entries.map((entry) {
               return GestureDetector(
                 onTap: () => _controller.animateToPage(entry.key),
                 child: Container(
@@ -66,8 +135,8 @@ class _MainBannerWidgetState extends State<MainBannerWidget> {
                   margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Colors.white.withOpacity(
-                        _current == entry.key ? 0.9 : 0.4),
+                    color: Colors.white
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4),
                   ),
                 ),
               );
