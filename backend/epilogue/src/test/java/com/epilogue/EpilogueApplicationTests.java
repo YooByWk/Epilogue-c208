3922c55e38d543c680f4b96510d76285c27c3f7a
tpwls101 <tpwls101@naver.com>
2024-03-18 13:59:33 +0900

    Test: EpilogueApplicationTests.java 주석 처리

--- backend/epilogue/src/test/java/com/epilogue/EpilogueApplicationTests.java
+++ backend/epilogue/src/test/java/com/epilogue/EpilogueApplicationTests.java
@@ -1,13 +1,13 @@
-package com.epilogue;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EpilogueApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.epilogue;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EpilogueApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
