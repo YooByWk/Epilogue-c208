6d5b35296f8e53ac336a258516f2aaf4f32ead2d
unknown <tls19190@naver.com>
2024-03-18 17:33:33 +0900

    Remove .dart_tool from tracking
    Former-commit-id: 62bfc0e7c2b7082e06c8f0266ac4c82586c31c19

--- frontend/lib/screens/login/login_viewmodel.dart
+++ frontend/lib/screens/login/login_viewmodel.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/login_model.dart';
 
 class LoginViewModel extends ChangeNotifier {
-  String _username = '';
-  String _password = '';
+  LoginModel _loginData = LoginModel(userId: '', password: '');
   bool _isFocused = false;
 
-  String get username => _username;
-  String get password => _password;
+  String get email => _loginData.userId;
+  String get password => _loginData.password;
   bool get isFocused => _isFocused;
 
-  void setUsername(String value) {
-    _username = value;
-    debugPrint('id : ' + value);
-    notifyListeners();
+  void setUserId(String value) {
+    _loginData = LoginModel(userId: value, password : _loginData.password);
   }
 
   void setPassword(String value) {
-    _password = value;
-    debugPrint('password : ' + value);
-    notifyListeners();
+    _loginData = LoginModel(userId : _loginData.userId, password : value); 
   }
 
   void setFocused(bool value) {
     _isFocused = value;
-    notifyListeners();
   }
 
-  void login() {
-    debugPrint('Username: $_username');
-    debugPrint('Password: $_password');
-    debugPrint('clicked login button!');
+  void login() async {
+    debugPrint('로그인 정보 : 아이디 ${_loginData.userId},  PW ${_loginData.password}');
   }
+
+
 }
