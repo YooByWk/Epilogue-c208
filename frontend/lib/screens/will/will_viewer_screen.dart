2cdb67e5a5fb455b026d9b38bde5e94ea739be4b
tlfahd2 <tlfahd2@naver.com>
2024-03-19 13:51:51 +0900

    Feat : 묘비명과 사진 업로드 화면 구성
    Former-commit-id: f1c96f59545f8599e39a8cf712c131bb9bc9be05

--- frontend/lib/screens/will/will_viewer_screen.dart
+++ frontend/lib/screens/will/will_viewer_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/main.dart';
+import 'package:frontend/screens/will/will_check_screen.dart';
 import 'package:frontend/screens/will/will_select_memorial_screen.dart';
+import 'package:frontend/widgets/will_viewer_widget.dart';
 
 class WillViewerScreen extends StatefulWidget {
   _WillViewerScreenState createState() => _WillViewerScreenState();
@@ -21,173 +25,41 @@ class _WillViewerScreenState extends State<WillViewerScreen> {
           children: [
             const Text("디지털 유언장 열람인 지정"),
             const Text("최대 5인 지정"),
-
             Column(
               children: List.generate(counter, (index) {
-                return Container(
-                  margin: EdgeInsets.only(
-                    top: 10,
-                  ),
-                  height: MediaQuery.of(context).size.height * 0.22,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: themeColour3.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Container(
-                          margin: EdgeInsets.only(top: 10, left: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '이름',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: themeColour4,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextFormField(
-                                style: TextStyle(
-                                  decorationThickness: 0,
-                                  fontSize: 20,
-                                  color: themeColour5,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: '이싸피',
-                                  hintStyle: TextStyle(color: themeColour5),
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '번호',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: themeColour4,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextFormField(
-                                style: TextStyle(
-                                  decorationThickness: 0,
-                                  fontSize: 20,
-                                  color: themeColour5,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: '010-1234-5678',
-                                  hintStyle: TextStyle(color: themeColour5),
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Container(
-                          margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '이메일',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: themeColour4,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextFormField(
-                                style: TextStyle(
-                                  decorationThickness: 0,
-                                  fontSize: 20,
-                                  color: themeColour5,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: 'ssafy@samsung.com',
-                                  hintStyle: TextStyle(color: themeColour5),
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '관계',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: themeColour4,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextFormField(
-                                style: TextStyle(
-                                  decorationThickness: 0,
-                                  fontSize: 20,
-                                  color: themeColour5,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: '가족',
-                                  hintStyle: TextStyle(color: themeColour5),
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFececec)),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+                // debugPrint('추가');
+                return WillViewerWidget();
               }),
             ),
-            ElevatedButton(onPressed: increment, child: Text('추가')),
+            Center(
+                child: (counter < 5)
+                    ? ElevatedButton(onPressed: increment, child: Text('추가'))
+                    : null),
+            Center(
+                child: (counter > 1)
+                    ? ElevatedButton(onPressed: delete, child: Text('삭제'))
+                    : null),
+            // ElevatedButton(onPressed: increment, child: Text('추가')),
             Row(
               children: [
                 TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("이전"),
+                  onPressed: () =>
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => WillCheckScreen(),
+                        ),
+                      ),
+                  child: const Text('이전'),
                 ),
                 TextButton(
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => WillSelectMemorialScreen(),
-                    ),
-                  ),
+                  onPressed: () =>
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => WillSelectMemorialScreen(),
+                        ),
+                      ),
                   child: const Text('다음'),
                 ),
               ],
@@ -202,6 +74,14 @@ class _WillViewerScreenState extends State<WillViewerScreen> {
     setState(() {
       if (counter < 5) {
         counter++;
+      }
+    });
+  }
+
+  void delete() {
+    setState(() {
+      if (counter > 1) {
+        counter--;
       }
     });
   }
