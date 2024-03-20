f79c39f895e35b90fee2ff709ca3a1704024fafa
tlfahd2 <tlfahd2@naver.com>
2024-03-20 11:16:54 +0900

    Style : 디지털 유언장 열람인 지정 화면
    Former-commit-id: b7a59ae3f11f9125d1bc6557acd56963ce820848

--- frontend/lib/screens/will/will_viewer_screen.dart
+++ frontend/lib/screens/will/will_viewer_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:frontend/main.dart';
 import 'package:frontend/screens/will/will_check_screen.dart';
 import 'package:frontend/screens/will/will_select_memorial_screen.dart';
+import 'package:frontend/screens/will/will_widgets.dart';
 import 'package:frontend/widgets/will_viewer_widget.dart';
 
 class WillViewerScreen extends StatefulWidget {
@@ -22,36 +25,48 @@ class _WillViewerScreenState extends State<WillViewerScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("디지털 유언장 열람인 지정"),
-            const Text("최대 5인 지정"),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 30.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextWidget(
+                          text: "디지털 유언장 \n열람인 지정",
+                          fontSize: 50,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ],
+                    ),
+                  ]),
+            ),
             Column(
               children: List.generate(counter, (index) {
-                // debugPrint('추가');
-                debugPrint(index.toString());
                 return Column(
                   children: [
                     WillViewerWidget(),
-                    Center(
+                    Container(
                         child: (index > 0)
                             ? ElevatedButton(
-                                onPressed: () => delete(index),
-                                child: Text('삭제'))
+                            onPressed: () => delete(index),
+                            child: Text('삭제'))
                             : null),
                   ],
                 );
               }),
             ),
-            Center(
+            Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10.0),
                 child: (counter < 5)
                     ? ElevatedButton(onPressed: increment, child: Text('추가'))
                     : null),
-            // Center(
-            //     child: (counter > 1)
-            //         ? ElevatedButton(onPressed: delete, child: Text('삭제'))
-            //         : null),
-            // ElevatedButton(onPressed: increment, child: Text('추가')),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 TextButton(
                   onPressed: () => Navigator.push(
