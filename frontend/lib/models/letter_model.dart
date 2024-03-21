12526ebe024de458d51d0f74e1e66fdc119ad7a9
unknown <tls19190@naver.com>
2024-03-21 12:01:40 +0900

    Feat : 추모관 편지 무한스크롤 view
    Former-commit-id: 2c757000b11db726e0e5fec5e0199465e5a1e11b

--- frontend/lib/models/letter_model.dart
+++ frontend/lib/models/letter_model.dart
@@ -2,12 +2,13 @@ class LetterModel {
   String title;
   String content;
   // String userId; // 사용자의 고유한 아이디, 이메일 혹은 전화번호로 사용자를 식별하는데 사용
-  String uderId = '실험용 유저 아이디';
+  String userId;
   DateTime date; // 작성일
 
   LetterModel({
     required this.title,
     required this.content,
     required this.date,
+    this.userId = '편지실험 아이디'
   });
   }
--- frontend/lib/view_models/memorial_view_models/memorial_detail_letterTab_viewmodel.dart
+++ frontend/lib/view_models/memorial_view_models/memorial_detail_letterTab_viewmodel.dart
@@ -4,28 +4,29 @@ import 'package:frontend/screens/memorial/memorial_detail/memorial_detail_letter
 import 'package:provider/provider.dart';
 
 class LetterViewModel extends ChangeNotifier {
-int _idx = 0;
   List<LetterModel> _letters = [];
+  int _idx = 0;
 
   List<LetterModel> get letters => _letters;
 
-  void updateLetter(LetterModel letter) { // 새로운 편지 불러오기
+  void updateLetter(LetterModel letter) {
+    // 새로운 편지 불러오기
     _letters.add(letter);
     notifyListeners();
   }
 
   LetterViewModel() {
     loadInitialData();
+    debugPrint('편지 뷰모델 생성');
   }
 
   void loadInitialData() {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 20; i++) {
       _letters.add(LetterModel(
         title: '편지 내용 $i',
         content: '편지 내용 $i',
-        date: DateTime.now(), 
-      )
-      );
+        date: DateTime.now(),
+      ));
       _idx++; // 편지 인덱스 증가
     }
     notifyListeners(); // 데이터가 변경되었음을 알림
@@ -33,15 +34,14 @@ int _idx = 0;
 
   Future<void> loadMore() async {
     // 데이터 로드
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 20; i++) {
       _letters.add(LetterModel(
         title: '편지 내용 $_idx',
         content: '편지 내용 $_idx',
         date: DateTime.now(),
-      )
-      );
+      ));
       _idx++;
     }
     notifyListeners();
-}
+  }
 }
