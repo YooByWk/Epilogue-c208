52b871c38e67131acdfaed85c8f0c6f787eb77eb
tlfahd2 <tlfahd2@naver.com>
2024-03-21 18:02:04 +0900

    Feat : 녹음 및 재생
    Former-commit-id: 90bf2679521b04a931c15a620aafba5ac753a7ce

--- frontend/lib/screens/login/login_screen.dart
+++ frontend/lib/screens/login/login_screen.dart
@@ -220,7 +220,7 @@ class LoginScreen extends StatelessWidget {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => Demo(),
+                              builder: (context) => RecordTest(),
                             ),
                           );
                         },
