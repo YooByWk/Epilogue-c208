61c73518d568f20b831372f5a061e0357cd5c6f9
tpwls101 <tpwls101@naver.com>
2024-03-22 21:19:21 +0900

    Feat: 디지털 추모관 방문 API 구현 중

--- backend/epilogue/src/main/java/com/epilogue/domain/memorial/MemorialVideo.java
+++ backend/epilogue/src/main/java/com/epilogue/domain/memorial/MemorialVideo.java
@@ -1,20 +1,23 @@
-//package com.epilogue.domain.memorial;
-//
-//import io.swagger.v3.oas.annotations.media.Schema;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//
-//@Entity
-//@Getter
-//public class MemorialVideo {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Schema(description = "동영상 식별키")
-//    private String videoSeq;
-//
-//    @Schema(description = "동영상 객체 url")
-//    private String videoURL;
-//}
+package com.epilogue.domain.memorial;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class MemorialVideo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "동영상 식별키")
+    private String videoSeq;
+
+    @Schema(description = "동영상 객체 url")
+    private String videoURL;
+
+    @Schema(description = "고인 식별키")
+    private int userSeq;
+}
