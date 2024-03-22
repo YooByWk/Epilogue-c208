37a681364c779a8b0bcc66c1411e0aae72aa7993
jaehyeon <hjaehyeon98@gmail.com>
2024-03-22 10:41:50 +0900

    Feat: 소셜로그인(네이버) 구현

--- backend/epilogue/src/main/java/com/epilogue/config/SecurityConfig.java
+++ backend/epilogue/src/main/java/com/epilogue/config/SecurityConfig.java
@@ -1,17 +1,37 @@
 package com.epilogue.config;
 
+import com.epilogue.service.CustomOAuth2UserService;
+import com.epilogue.service.CustomSuccessHandler;
+import com.epilogue.util.JWTFilter;
+import com.epilogue.util.JWTUtil;
+import com.epilogue.util.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,14 +48,28 @@ public class SecurityConfig {
         http
                 .httpBasic(AbstractHttpConfigurer::disable);
 
-        // 경로별 인가 작업
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/**", "/swagger-ui/**", "/api-docs/**", "/api/test", "/api/authenticate", "/api", "/api/user/join/**", "/login", "/api/auth/**", "/api/oauth2/**", "/api/memorial/list").permitAll()
-                .anyRequest().authenticated());
-
-        // oauth2
+        //JWTFilter 추가
         http
-                .oauth2Login(Customizer.withDefaults());
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        //oauth2
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                );
+
+        // 커스텀 필터 등록
+        // (생성한 커스텀 필터, 필터를 넣을 위치)
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), LoginFilter.class);
+
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
 
         //세션 설정 : STATELESS
         http
--- backend/epilogue/src/main/java/com/epilogue/service/CustomOAuth2UserService.java
+++ backend/epilogue/src/main/java/com/epilogue/service/CustomOAuth2UserService.java
@@ -20,9 +20,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!");
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        System.out.println(oAuth2User);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response;
