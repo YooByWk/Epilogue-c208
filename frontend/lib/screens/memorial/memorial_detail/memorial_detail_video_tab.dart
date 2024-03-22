9dace002312014a7fbc8c2e20c0de478e14a0abc
unknown <tls19190@naver.com>
2024-03-22 10:21:51 +0900

    Docs : 제출 readme
    Former-commit-id: e651adb2736fbb273572b1e0724636915b2b937a

--- frontend/lib/screens/memorial/memorial_detail/memorial_detail_video_tab.dart
+++ frontend/lib/screens/memorial/memorial_detail/memorial_detail_video_tab.dart
@@ -41,7 +41,6 @@ class VideoTab extends StatelessWidget {
                             onPressed: () {
                               debugPrint('비디오 업로드 버튼 클릭');
                               Navigator.pushNamed(context,'/blockChain');
-                              ;
                             }),
                         SizedBox(height: 10)
                       ]));
