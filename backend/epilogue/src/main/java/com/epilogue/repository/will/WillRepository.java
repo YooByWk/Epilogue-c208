bb5b5e4e21424540e97d32b929d218162e15f94f
kn9012 <kn9012@naver.com>
2024-03-25 14:36:49 +0900

    Chore: WillRepository 수정

--- backend/epilogue/src/main/java/com/epilogue/repository/will/WillRepository.java
+++ backend/epilogue/src/main/java/com/epilogue/repository/will/WillRepository.java
@@ -1,12 +1,16 @@
 package com.epilogue.repository.will;
 
 import com.epilogue.domain.will.Will;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface WillRepository extends JpaRepository<Will, Integer> {
     void deleteByWillSeq(int willSeq);
 
+    @Modifying
+    @Transactional
     @Query("UPDATE Will w SET w.willFileAddress = :willFileAddress WHERE w.willSeq = :willSeq")
     void updateWillFileAddress(int willSeq, String willFileAddress);
 }
