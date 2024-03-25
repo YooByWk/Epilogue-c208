c0eeeea9cf22a8db0d0dc6d87051f94b1d54dcc7
tpwls101 <tpwls101@naver.com>
2024-03-25 16:09:12 +0900

    Feat: 디지털 추모관 사진 및 영상 업로드 API 구현

---
+++ backend/epilogue/src/main/java/com/epilogue/dto/request/memorial/MemorialMediaRequestDto.java
@@ -0,0 +1,17 @@
+package com.epilogue.dto.request.memorial;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Schema(description = "디지털 추모관 사진/영상 업로드 요청 DTO")
+public class MemorialMediaRequestDto {
+    @NotNull
+    @Schema(description = "사진/영상 파일")
+    private MultipartFile multipartFile;
+
+    @Schema(description = "설명 내용")
+    private String content;
+}
