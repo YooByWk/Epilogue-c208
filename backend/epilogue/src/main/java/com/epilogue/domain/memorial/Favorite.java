f5c4457b95b97fbe1c59feacf06cfd8571c86c36
tpwls101 <tpwls101@naver.com>
2024-03-25 10:37:13 +0900

    Update: 서버 에러 해결

--- backend/epilogue/src/main/java/com/epilogue/domain/memorial/Favorite.java
+++ backend/epilogue/src/main/java/com/epilogue/domain/memorial/Favorite.java
@@ -1,5 +1,6 @@
 package com.epilogue.domain.memorial;
 
+import com.epilogue.domain.user.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
