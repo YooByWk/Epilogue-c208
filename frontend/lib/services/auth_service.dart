e3ff38e6f11fb9fedcd0d35027caf90d5f2da03b
kanagyi95 <kanagyi95@gmail.com>
2024-03-25 10:26:18 +0900

    Chore: debugPrint 추가
    Former-commit-id: 60e785a377ed24578ba6738b11b2a8336b6725e1

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -3,6 +3,8 @@ import 'package:dio/dio.dart';
 import 'package:frontend/models/login_model.dart';
 import 'package:frontend/models/signup_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
+
 
 class AuthService {
   final Dio _dio = Dio();
@@ -21,6 +23,7 @@ class AuthService {
         return handler.next(options); // 요청을 계속 진행한다
       },
     ));
+    _dio.interceptors.add(LoggingInterceptor());  // console에 로깅
   }
 
   ///////////////////////// 로그인 //////////////////////////////////
@@ -80,20 +83,20 @@ class AuthService {
 class LoggingInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print("REQUEST[${options.method}] => PATH: ${options.path}");
+    debugPrint("REQUEST[${options.method}] => PATH: ${options.path}");
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    print(
+    debugPrint(
         "RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}");
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    print(
+    debugPrint(
         "ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}");
     super.onError(err, handler);
   }
