a6add5bb0e1eaf0c699f5b972a6fa28bb60aeead
jaehyeon <hjaehyeon98@gmail.com>
2024-03-29 11:10:34 +0900

    Refactor: 토큰 발급 key 수정

--- backend/epilogue/src/main/java/com/epilogue/util/CustomSuccessHandler.java
+++ backend/epilogue/src/main/java/com/epilogue/util/CustomSuccessHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -22,6 +23,7 @@ import java.util.Iterator;
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JWTUtil jwtUtil;
+    private final StringRedisTemplate redisTemplate;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -34,21 +36,18 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String role = auth.getAuthority();
 
         //토큰 설정
-        String token = jwtUtil.createAccessToken(username, role);
 
-        //응답 설정
-        Cookie cookie = createCookie(token);
-        response.addCookie(cookie);
-        log.info("response={}", response);
-        response.setStatus(HttpStatus.OK.value());
+        String accessToken = jwtUtil.createAccessToken(username, role); // access token
+        String refreshToken = jwtUtil.createRefreshToken(username, role); // refresh token
+
+        // Bearer 인증 방식
+        // 응답 헤더에 JWT 토큰 값을 넣어 응답
+        // 응답 헤더에서 "Authorization" key 값에 "Bearer ~~" 값을 확인할 수 있음 (JWT)
+        response.addHeader(JWTUtil.ACCESS_TOKEN, "Bearer " + accessToken);
+
+        // Redis refreshToken 저장
+        redisTemplate.opsForValue().set(JWTUtil.REFRESH_TOKEN, "Bearer " + refreshToken);
+
     }
 
-    private Cookie createCookie(String value) {
-        Cookie cookie = new Cookie("Authorization", value);
-        cookie.setMaxAge(24 * 60 * 60);
-        //cookie.setSecure(true);
-        //cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        return cookie;
-    }
 }
