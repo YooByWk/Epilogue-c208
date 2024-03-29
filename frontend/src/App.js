1ffc7bb990be75d376f84c4994a58cf0359e406c
tlfahd2 <tlfahd2@naver.com>
2024-03-29 16:50:23 +0900

    Feat : build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Select from "./Select";
 import WillApplyWitness from './WillApplyWitness';
-
+import WillViewViewer from './WillViewViewer';
 function App() {
   return (
     <>
@@ -11,6 +11,7 @@ function App() {
       <Route path='home' element={<Select />} />
       <Route path="/" element={<Select />} />
       <Route path="/witness" element={<WillApplyWitness />} />
+      <Route path='/viewer' element={<WillViewViewer />} />
     </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/WillViewViewer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function WillViewViewer() {
+  const [code, setCode] = useState('');
+
+  const handleSubmit = async () => {
+    if (code.trim() === '') {
+      alert('코드를 입력해주세요.');
+      return;
+    }
+
+    try {
+      const response = await axios.post('URL_HERE', { code });
+
+      // Handle response as needed
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>유언장 확인하기</h1>
+      <p>유언장을 확인하기 위해 전달 받은 코드를 입력해주세요.</p>
+      <input
+        type="text"
+        placeholder="보안코드"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button onClick={handleSubmit}>제출하기</button>
+    </div>
+  );
+}
+
+export default WillViewViewer;
