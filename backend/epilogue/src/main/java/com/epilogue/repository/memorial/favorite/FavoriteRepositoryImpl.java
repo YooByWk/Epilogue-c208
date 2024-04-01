3f9223fa4827dec09ee84491d30ab897487cc628
tpwls101 <tpwls101@naver.com>
2024-04-01 21:03:17 +0900

    Refactor: 디지털 추모관 즐겨찾기 수정

--- backend/epilogue/src/main/java/com/epilogue/repository/memorial/favorite/FavoriteRepositoryImpl.java
+++ backend/epilogue/src/main/java/com/epilogue/repository/memorial/favorite/FavoriteRepositoryImpl.java
@@ -14,13 +14,6 @@ public class FavoriteRepositoryImpl implements FavoriteRepositoryCustom {
     EntityManager entityManager;
 
     @Override
-    public List<Favorite> findListById(int userSeq) {
-        return entityManager.createQuery("SELECT f FROM Favorite f JOIN FETCH f.memorial WHERE f.memorial.user.userSeq = :userSeq ORDER BY f.memorial.goneDate DESC", Favorite.class)
-                .setParameter("userSeq", userSeq)
-                .getResultList();
-    }
-
-    @Override
     public List<Favorite> findByUserId(String loginUserId) {
         return entityManager.createQuery("SELECT f FROM Favorite f WHERE f.user.userId = :loginUserId", Favorite.class)
                 .setParameter("loginUserId", loginUserId)
