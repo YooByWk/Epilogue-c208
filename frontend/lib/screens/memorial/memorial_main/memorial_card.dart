627e4b5b9cbfba83dda2c1cd0bbfb3a7a0b399e9
tlfahd2 <tlfahd2@naver.com>
2024-04-01 20:53:40 +0900

    Design : 추모관 리스트
    Former-commit-id: 20fbf32a1a9c478a399a4726894f0e68483ec426

--- frontend/lib/screens/memorial/memorial_main/memorial_card.dart
+++ frontend/lib/screens/memorial/memorial_main/memorial_card.dart
@@ -4,6 +4,24 @@ import 'package:frontend/view_models/memorial_view_models/memorial_list_viewmode
 import 'package:frontend/widgets/common_button.dart';
 
 class MemorialCard extends StatelessWidget {
+// 글자수 길면 잘라버리기
+  String _truncateText(String text, int maxLines) {
+    final span = TextSpan(text: text);
+    final tp = TextPainter(
+      text: span,
+      maxLines: maxLines,
+      textDirection: TextDirection.ltr,
+    );
+
+    tp.layout();
+
+    if (tp.didExceedMaxLines) {
+      final endIndex = tp.getPositionForOffset(Offset(0, tp.height)).offset;
+      return text.substring(0, endIndex - 3) + '...';
+    } else {
+      return text;
+    }
+  }
 
   final _storage = FlutterSecureStorage();
   // final String imagePath;
@@ -34,8 +52,8 @@ class MemorialCard extends StatelessWidget {
           CommonButtonWidget(
             height: 300,
             width: 300,
-            text: '$graveName\n故$name',
-            fontSize: 24,
+            text: _truncateText('$graveName', 2),
+            fontSize: 15,
             textColor: Colors.black,
             imagePath: 'assets/images/stone.png',
             onPressed: () {
@@ -61,6 +79,10 @@ class MemorialCard extends StatelessWidget {
                 });
                 },
             ),
+          ),
+          Positioned(
+            bottom: 30,
+            child: Text('故$name', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
           ),
         ],
       ),
