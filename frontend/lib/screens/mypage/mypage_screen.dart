2dd330cee4eba404d21a1aa23526951419533c50
unknown <tls19190@naver.com>
2024-04-01 18:04:29 +0900

    git ; 오류..
    Former-commit-id: 5a5a71862be24d85992cf95c1813b9ce745dc3d3

--- frontend/lib/screens/mypage/mypage_screen.dart
+++ frontend/lib/screens/mypage/mypage_screen.dart
@@ -3,119 +3,141 @@ import 'package:frontend/main.dart';
 import 'package:frontend/screens/mypage/mypage_modify_info_screen.dart';
 import 'package:frontend/screens/mypage/mypage_will_more_screen.dart';
 import 'package:frontend/view_models/auth_view_models/user_viewmodel.dart';
+import 'package:frontend/view_models/will_view_models/my_will_viewmodel.dart';
 import 'package:frontend/widgets/common_button.dart';
 import 'package:frontend/widgets/common_text_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/screens/login/login_screen.dart';
 
-class MypageScreen extends StatelessWidget {
+class MypageScreen extends StatefulWidget {
+  const MypageScreen({Key? key}) : super(key: key);
+  @override 
+  _MypageScreenState createState() => _MypageScreenState();
+}
+  class _MypageScreenState extends State<MypageScreen> {
+
+  late Future myWillDataFuture;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    myWillDataFuture = MyWillViewModel().normalfetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: CommonText(
-            text: '내 공간',
-            isBold: true,
-            fontSize: 40,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserViewModel()),
+        ChangeNotifierProvider(create: (context) => MyWillViewModel()),
+      ],
+      child: Scaffold(
+          appBar: AppBar(
+            title: CommonText(
+              text: '내 공간',
+              isBold: true,
+              fontSize: 40,
+            ),
+            automaticallyImplyLeading: false,
           ),
-          automaticallyImplyLeading: false,
-        ),
-        body: Consumer<UserViewModel>(builder: (context, viewModel, child) {
-          if (viewModel.isLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (viewModel.errorMessage != null) {
-            // await viewModel.fetchUserData();
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(child: Text(viewModel.errorMessage!)),
-                SizedBox(height: 20),
-                CommonButtonWidget(text: '홈으로', onPressed: ()=>Navigator.pushNamed(context, '/home').then((value) async => debugPrint('홈으로 이동 후 토큰 ${await storage.read(key: 'token')}')) ),
-                SizedBox(height: 20),
-                CommonButtonWidget(text: '로그아웃', onPressed: () async {
-                  await viewModel.logout();
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()),
-                      (Route<dynamic> route) => false);
-                }),
-
-              ],
-            );
-          } else if (viewModel.user == null) {
-            return Center(child: Text('사용자 정보를 불러올 수 없습니다.'));
-          }
-          final user = viewModel.user!;
-          return SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          body: Consumer2<UserViewModel, MyWillViewModel>(builder: (context, viewModel, myWillViewModel, child) {
+            if (viewModel.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (viewModel.errorMessage != null) {
+              // await viewModel.fetchUserData();
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CommonText(
-                    text: '${user.name}님',
-                    fontSize: 30,
-                    isBold: true,
-                  ),
-                  CommonButtonWidget(
-                      text: '정보 수정',
-                      width: 100,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Provider<UserViewModel>(
-                                      create: (context) => UserViewModel(),
-                                      child: MypageModifyInfoScreen(),
-                                    )));
-                      }),
-                  CommonButtonWidget(
-                      text: '로그아웃',
-                      width: 100,
-                      backgroundColor: themeColour3,
-                      onPressed: () async {
-                        await viewModel.logout();
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(builder: (context) => LoginScreen()),
-                                (Route<dynamic> route) => false);
-                      }),
+                  Center(child: Text(viewModel.errorMessage!)),
+                  SizedBox(height: 20),
+                  CommonButtonWidget(text: '홈으로', onPressed: ()=>Navigator.pushNamed(context, '/home').then((value) async => debugPrint('홈으로 이동 후 토큰 ${await storage.read(key: 'token')}')) ),
+                  SizedBox(height: 20),
+                  CommonButtonWidget(text: '로그아웃', onPressed: () async {
+                    await viewModel.logout();
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                        (Route<dynamic> route) => false);
+                  }),
+      
                 ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 30),
+              );
+            } else if (viewModel.user == null) {
+              return Center(child: Text('사용자 정보를 불러올 수 없습니다.'));
+            }
+            final user = viewModel.user!;
+            return SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    SizedBox(height: 40),
                     CommonText(
-                      text: '내가 기록한 유언',
-                      fontSize: 24,
+                      text: '${user.name}님',
+                      fontSize: 30,
                       isBold: true,
                     ),
-                    SizedBox(height: 20),
                     CommonButtonWidget(
-                        height: 200,
-                        width: 200,
-                        borderColor: themeColour5,
-                        text: '더보기',
-                        fontSize: 24,
-                        textColor: Colors.black,
-                        imagePath: 'assets/images/willmore.png',
+                        text: '정보 수정',
+                        width: 100,
                         onPressed: () {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) =>
-                                      MypageWillMoreScreen()));
+                                  builder: (context) => Provider<UserViewModel>(
+                                        create: (context) => UserViewModel(),
+                                        child: MypageModifyInfoScreen(),
+                                      )));
                         }),
-                    SizedBox(height: 40),
+                    CommonButtonWidget(
+                        text: '로그아웃',
+                        width: 100,
+                        backgroundColor: themeColour3,
+                        onPressed: () async {
+                          await viewModel.logout();
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(builder: (context) => LoginScreen()),
+                                  (Route<dynamic> route) => false);
+                        }),
                   ],
                 ),
-              )
-            ],
-          ));
-        }));
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 40),
+                      CommonText(
+                        text: '내가 기록한 유언',
+                        fontSize: 24,
+                        isBold: true,
+                      ),
+                      SizedBox(height: 20),
+                      CommonButtonWidget(text: 'text', onPressed: () => debugPrint(myWillViewModel.graveType.toString())),
+                      CommonButtonWidget(
+                          height: 200,
+                          width: 200,
+                          borderColor: themeColour5,
+                          text: '더보기',
+                          fontSize: 24,
+                          textColor: Colors.black,
+                          imagePath: 'assets/images/willmore.png',
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        MypageWillMoreScreen()));
+                          }),
+                      SizedBox(height: 40),
+                    ],
+                  ),
+                )
+              ],
+            ));
+          })),
+    );
   }
 }
