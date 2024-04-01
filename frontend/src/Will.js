b60b5dd7d2f10de607c412459d7536a75039c1dc
unknown <tls19190@naver.com>
2024-04-01 11:45:42 +0900

    Feat : 리액트 블록체인 호출 성공 : logs

--- frontend/src/Will.js
+++ frontend/src/Will.js
@@ -1,8 +1,17 @@
+
+import BlockChainHook from './hooks/blockChain';
+
+
 function Will() {
+    const blockChainHook = BlockChainHook();
     return(
         <>
+        <h1>유언장</h1>
             <video src="7d3a2056-371b-4329-8fa8-815caee56054.mp4" alt="비디오"></video>
+            
+            
         </>
+        
     )
 }
 
