57d41c78ada1a9da1d763326da3f1c782bcfc3b4
tlfahd2 <tlfahd2@naver.com>
2024-04-04 12:29:42 +0900

    Git : flutter 최종
    Former-commit-id: 1c43230fff131516f41a2d9745ed615311b7b4e0

---
+++ frontend/build/flutter_naver_login/generated/source/buildConfig/debug/com/yoonjaepark/flutter_naver_login/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yoonjaepark.flutter_naver_login;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.yoonjaepark.flutter_naver_login";
+  public static final String BUILD_TYPE = "debug";
+}
