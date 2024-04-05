6c0e0d957fd7f3b4ddb942f580e56c90a4adc25f
kanagyi95 <kanagyi95@gmail.com>
2024-04-05 15:47:48 +0900

    필요없는거 del
    Former-commit-id: e986a92d2fb60be262ce588c1a1b19ad821d0930

--- backend/epilogue/src/main/java/com/epilogue/repository/memorial/letter/MemorialLetterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epilogue.repository.memorial.letter;
-
-import com.epilogue.domain.memorial.MemorialLetter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemorialLetterRepository extends JpaRepository<MemorialLetter, Integer>, MemorialLetterRepositoryCustom {
-}
