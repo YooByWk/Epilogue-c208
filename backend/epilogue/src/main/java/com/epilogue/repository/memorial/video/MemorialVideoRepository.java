6dc81c30aeab95709ef20e533835b3caff75710e
kanagyi95 <kanagyi95@gmail.com>
2024-04-05 14:57:24 +0900

    힝..
    Former-commit-id: eb394c8349710fdc016720b47b2e6fe0eb7d3c5b

--- backend/epilogue/src/main/java/com/epilogue/repository/memorial/video/MemorialVideoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epilogue.repository.memorial.video;
-
-import com.epilogue.domain.memorial.MemorialVideo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemorialVideoRepository extends JpaRepository<MemorialVideo, Integer>, MemorialVideoRepositoryCustom {
-}
